cd010c64aed746945b2342cd4b8badbabba45d63
asasasas <aaaaaaa@unal.edu.co>
2024-02-22 16:54:04 -0500

    final_espero_si

--- src/gestionHumana/Empleado.java
+++ src/gestionHumana/Empleado.java
@@ -2,13 +2,9 @@ package gestionHumana;
 
 public class Empleado {
     public final long cedula;
-<<<<<<< HEAD
-    private String nombre;
-    private String cargo;
-=======
+
     public String nombre;
     public String cargo;
->>>>>>> main
 
     public Empleado(long cedula, String nombre, String cargo) {
         this.cedula = cedula;
