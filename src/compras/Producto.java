b8e62aac74673c2264734a1c8a3af2253424c91e
Origimed <allopezp@unal.edu.co>
2024-02-22 10:43:01 -0500

    primero

---
+++ src/compras/Producto.java
@@ -0,0 +1,30 @@
+package compras;
+
+public class Producto {
+    private final int codigo;
+    private String nombre;
+    private String tipo;
+    private int totalProductosPedidos;
+
+    Producto(int codigo, String nombre, String tipo) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    void imprimirNombre() {
+        System.out.print(nombre);
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    int getTotalProductosPedidos() {
+        return totalProductosPedidos;
+    }
+}
